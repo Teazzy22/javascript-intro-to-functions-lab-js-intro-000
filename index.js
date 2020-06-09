ec2e2e8d25555f270a773ea08363f9b56960886a
Kyle Nugent <kln5656@gmail.com>
2020-06-09 19:49:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 function shout(string) {
   return string.toUpperCase()
 }
-function logShout(string.toUpperCase) {
-  console.log(string);
-}
+
 function whisper(string) {
   return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
 }
