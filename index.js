9f70c8e58906a6022dfcc06401d3611a032e3a9b
Kyle Nugent <kln5656@gmail.com>
2020-06-09 20:01:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,6 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma (string) {
-  return "I can't hear you!" ? string === string.toLowerCase()
+  return "I can't hear you!"
+  string.toLowerCase() === shout(string)
 }
