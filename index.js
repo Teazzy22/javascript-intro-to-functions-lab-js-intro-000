5768e4d58736810448d220e230d14015958469cf
Kyle Nugent <kln5656@gmail.com>
2020-06-09 20:29:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string()) {
+  if (string.toLowerCase() === string) {
   return 'I can\'t hear you';
 }
-  if (string.toUpperCase() === string()) {
+  if (string.toUpperCase() === string) {
   return 'YES INDEED!';
 }
-  if (string.mixedCase() === string()){
+  if (string.mixedCase() === string){
   return '';
 }
 }
